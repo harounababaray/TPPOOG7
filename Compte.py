f2ec48ca74bb848b4023b0fbbff5ffac408c4ad0
ahmatdoumguitte <57494524+ahmatdoumguitte@users.noreply.github.com>
2019-11-10 15:11:02 +0100

    Compte
    Client

--- Compte.py
+++ Compte.py
@@ -1,1 +1,20 @@
+Class compte:
+     def_init_(self, compteIndividus,compteJoint) :
+          Clients._init_(self,compteIndividus, compteJoint, compteTitre)#appelle a notre classe maire
+         
+          self.compteIndividus=compteIndividus
+          self.compteJoint=compteJoint
+          self.compteTitre=compteTitre
+          self. CompteAterme=compteAterme
+          return "compteIndividus{},compteJoint{},compteTitre{},compteAterme{}". format(self.compteIndividus,self.compteJoint, self.compteTitre,sel.fcompteAterme)
+      
 
+
+
+      
+
+
+
+
+     #AHMAT ADOUM IDRISS 
+     MATRICULE:18B004FS
