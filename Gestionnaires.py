95c6961694b9e343bdc0b9562478976cd80e3661
harounababaray <57563363+harounababaray@users.noreply.github.com>
2019-11-17 21:04:25 +0100

    Update Gestionnaires.py

--- Gestionnaires.py
+++ Gestionnaires.py
@@ -1,1 +1,13 @@
 
+class Gestionnaires(Banque):
+    def __init__(self,nom,tel,email):
+        self.nom= nom
+        self.tel= tel
+        self.email= email
+        return "gestionnaire{0},nimero telephone{1},compte email{2}".format(self.nom,self.tel,self.email)
+        gestionnaire=gestionnaire("Mahamat Ahmat",+10000000000,m@email.com)
+        Banque.__init__(self,nom,email)#qui fait l'appel au constructeur de la classe mére
+        self.tel=tel
+
+Nom: Harouna
+Matricule :18A854FS
