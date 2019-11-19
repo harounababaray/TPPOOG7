3a725b9476b000070959cb4ae70d92c39ce6b905
harounababaray <57563363+harounababaray@users.noreply.github.com>
2019-11-19 14:43:52 +0100

    Update Gestionnaires.py

--- Gestionnaires.py
+++ Gestionnaires.py
@@ -1,16 +1,2 @@
 
-classe  Gestionnaires ( Banque ):
-    def  __init__ ( self , nom , tel , email ):
-        auto .nom = nom
-        auto .tel = tel
-        self .email = email
-        retourne  " gestionnaire {0} , nimero telephone {1} , compte email {2} " .format ( self .nom, self .tel, self .email)
-        gestionnaire = gestionnaire ( " Mahamat Ahmat " , + 10000000000 , m @ email.com)
-Banque. __init__ ( self , nom, email) # qui a fait l'appel à la classe ouvrière
-        auto .tel = tel
-
-
-
-#Nom:Harouna
-#Matricule:18A854FS 
         
